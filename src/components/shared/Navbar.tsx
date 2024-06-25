65e7eda531545ab8ccae4730fa679d9513d367be
shinhojung <shinhojung814@gmail.com>
2024-06-26 00:50:58 +0900

    Added icons

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -37,7 +37,9 @@ function Navbar() {
 
   return (
     <Flex justify="space-between" align="center" css={navbarContainerStyles}>
-      <Link to="/">홈</Link>
+      <Link to="/">
+        <IconHome />
+      </Link>
       {renderButton()}
     </Flex>
   )
@@ -51,5 +53,41 @@ const navbarContainerStyles = css`
   background-color: ${colors.white};
   z-index: 10;
 `
+
+function IconHome() {
+  return (
+    <svg
+      version="1.1"
+      viewBox="0 0 20 19"
+      width={24}
+      height={24}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title />
+      <desc />
+      <defs />
+      <g
+        fill="none"
+        fill-rule="evenodd"
+        id="Page-1"
+        stroke="none"
+        stroke-width="1"
+      >
+        <g
+          fill="#000000"
+          id="Core"
+          transform="translate(-506.000000, -255.000000)"
+        >
+          <g id="home" transform="translate(506.000000, 255.500000)">
+            <path
+              d="M8,17 L8,11 L12,11 L12,17 L17,17 L17,9 L20,9 L10,0 L0,9 L3,9 L3,17 L8,17 Z"
+              id="Shape"
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
 
 export default Navbar
